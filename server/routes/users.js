b720c33497452d5836182a8d832f96ebfff73f29
Felix Michel <felix.michel@tum.de>
2017-03-29 14:25:35 +0200

    refacotrign

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ import express from 'express';
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next)=>{
   [1,2,3].map(n => n + 1);
   res.send('respond with a resource');
 });
