d0bd3ef53a4d8ef402f967e39f99854e82cd29b5
Felix Michel <felix.michel@tum.de>
2017-03-30 13:55:57 +0200

    unused packages removed

--- app/routes/route.app.js
+++ app/routes/route.app.js
@@ -4,9 +4,7 @@ import stagedefinition from './route.stagedefinition';
 import importer from './route.import';
 import test from './route.test';
 
-
 function routes(){
-    console.log('here');
     const router = express.Router();
     router.use('/casedefinition', casedefinition);
     router.use('/casedefinitions', casedefinition);
@@ -14,8 +12,7 @@ function routes(){
     router.use('/stagedefinitions', stagedefinition);
     router.use('/import', importer);
     router.use('/test', test);
-     console.log('heree');
-     return router;
+    return router;
 };
 
 export default routes;
