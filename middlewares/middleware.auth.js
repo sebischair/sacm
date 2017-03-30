f0b65cd381f952f0873913a93dda47d277d7ec10
Patrick Holl <patrick.holl@tum.de>
2017-03-30 12:57:08 +0200

    fix

--- middlewares/middleware.auth.js
+++ middlewares/middleware.auth.js
@@ -5,21 +5,8 @@ import BasicStrategy from 'passport-http';
 module.exports =
 function Auth(param) {
 
-  // Define strategy
-  passport.use(new BasicStrategy(
-    function(userid, password, done) {
-
-      if(password === '123') {
-        return done(null, {id: 2, name: 'hans'});
-      }
-      return done(null, false);
-
-    }
-  ));
-
 
   return function (req, res, next) {
-    passport.authenticate('basic', { session: false });
     return next();
   };
 
