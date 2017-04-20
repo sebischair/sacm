756b4181e33399aade70ff01685d427911f5f170
Felix Michel <felix.michel@tum.de>
2017-04-20 10:15:02 +0200

    message model added

---
+++ app/models/case/model.message.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import Promise from 'bluebird';
+import http from '../http';
+import Model from '../model';
+
+
+export default class Message extends Model{
+
+  static create(data) {
+    return http.post('case/'+data.case+'/messages', data);
+  }
+
+  static findByCaseId(caseId) {
+    return http.get('case/'+caseId+'/messages');
+  }
+
+  static findById(messageId) {
+    return http.get('message/'+messageId);
+  }
+
+  static deleteById(messageId) {
+    return http.del('message/'+messageId);
+  }
+
+}
