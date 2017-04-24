820e480ed1ffc88977b1e92bf9a30ceaa847b7a5
Felix Michel <felix.michel@tum.de>
2017-04-24 16:09:42 +0200

    ownerPath added for casedefintion and humantask

--- app/importer/xmlimporter.js
+++ app/importer/xmlimporter.js
@@ -192,6 +192,7 @@ module.exports = class XMLImporter {
             const data = {
               name: cd.$.id,
               label: cd.$.label,
+              ownerPath: cd.$.ownerPath,
               entityDefinition: {id: entityDefinitionId}
             };
             return CaseDefinition.create(data)
@@ -293,7 +294,8 @@ module.exports = class XMLImporter {
       return Promise.each(humanTaskDefinitions, td=>{        
         const data = {
           name: td.$.id,
-          label: td.$.id,
+          label: td.$.id,          
+          ownerPath: td.$.ownerPath,
           isRepeatable: td.$.isRepeatable,
           isMandatory: td.$.isMandetory,
           caseDefinition: {id: caseDefId},
