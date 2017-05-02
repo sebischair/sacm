565253cdd6e21134c225b0ba23a6e2fd2893a6c3
Felix Michel <felix.michel@tum.de>
2017-05-02 09:54:33 +0200

    http request refactored

--- app/models/http.js
+++ app/models/http.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var config = require('../../config');
 var request = require('request');
+import rq from 'request-promise';
 var Promise = require('bluebird');
 var colors = require('colors');
 
@@ -19,6 +20,16 @@ function printRequest(method, url, reqBody, resBody, statusCode){
         console.log(reqBody);
         console.log(resBody);
     }
+}
+
+function successRequest(method, url, reqBody, resBody, statusCode){
+    console.log('SC-'+method+': '+ url + " "+colors.green(statusCode));
+}
+
+function errorRequest(method, url, reqBody, resBody, statusCode){
+    console.log(colors.red('SC-'+method+': '+ url + " "+colors.green(statusCode)));
+    console.log(reqBody);
+    console.log(resBody);
 }
 
 console.log(headers.Authorization);
@@ -41,21 +52,25 @@ module.exports = {
         });
     },
     post: function(path, data){
+        console.log('###### POST 2 #######');
+        console.log(JSON.stringify(data));
+        console.log('###### POST 2 END #######');
         return new Promise(function (resolve, reject){
-            console.log('###### POST 2 #######');
-            console.log(JSON.stringify(data));
-            console.log('###### POST 2 END #######');
-            request.post({
-                url: config.sc.url + path,
+            rq.post({
+                uri: config.sc.url +path,
                 headers: headers,
-                json: data
-            }, function(err, res, body){
-                printRequest('POST', config.sc.url+path, data, body, res!=undefined? res.statusCode: '');
-                if(err || res.statusCode != 200)
-                    reject(body);
-                else
-                    resolve(body)
-            });
+                body: data,
+                json: true,
+                resolveWithFullResponse: true 
+            })
+            .then(res=>{
+                successRequest('POST', config.sc.url+path, data, res.body, res.statusCode);
+                resolve(res.body);
+            })
+            .catch(res=>{
+                errorRequest('POST', config.sc.url+path, data, res.error, res.statusCode);
+                reject(res.error);
+            });          
         });
     },
     put: function(path, data, cb){
