fb649ec37b55708397a69c6423184d0037ca59ba
Felix Michel <felix.michel@tum.de>
2017-05-15 00:08:22 +0200

    userDefinition added

---
+++ app/models/casedefinition/model.userdefinition.js
@@ -0,0 +1,12 @@
+import Promise from 'bluebird';
+import http from '../http';
+import Model from '../model';
+
+
+export default class UserDefinition extends Model{
+
+  static find() {
+    return http.get('/userType');
+  }
+  
+}
