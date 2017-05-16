4a9621a9b3545335018b4cab9d7961509dd3d3dc
Patrick Holl <patrick.holl@tum.de>
2017-05-16 16:42:01 +0200

    Added base config

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    sc : {
+        url: 'http://sacm_sociocortex_1:8083/intern/tricia/api/v1',
+        pass : 'ottto',
+        user : 'mustermann@test.sc'
+    }
+ };
+
+ //   http://scdockerprod_app_1:8083/intern/tricia/api/v1
