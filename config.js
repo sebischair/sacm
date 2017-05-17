96c52900d4b8e0da01bab380cc81ee9dfe3a2059
Patrick Holl <patrick.holl@tum.de>
2017-05-17 09:20:05 +0200

    Config fix

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     sc : {
-        url: 'http://sacm_sociocortex_1:8083/intern/tricia/api/v1',
+        url: 'http://sacm_sociocortex_1:8083/api/v1',
         pass : 'ottto',
         user : 'mustermann@test.sc'
     }
