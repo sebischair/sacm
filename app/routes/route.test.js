d6c4da859fa3fc2b2dd03a013280077fa5a25b77
Felix Michel <felix.michel@tum.de>
2017-05-21 16:44:10 +0200

    fix

--- app/routes/route.test.js
+++ app/routes/route.test.js
@@ -19,8 +19,14 @@ import Case from '../models/case/model.case';
 import HumanTask from '../models/case/model.humantask';
 
 router.get('/', (req, res, next)=>{
-  console.log('test')
-  let workspace = null;
+
+  let workspace = null;  
+  let rootEntityDefinition = null;
+  let caseDefinition = null;
+  let stageDefinitioIdentification = null;
+  let humanTaskDefinitionIdentification = null;
+
+  let case2 = null;
 
   return Workspace.deleteAll()
     .then(()=>{
@@ -30,8 +36,65 @@ router.get('/', (req, res, next)=>{
     })
     .then(persistedWorkspace=>{
       workspace = persistedWorkspace;
-      console.log('testworkspace')
+      return EntityDefinition.create2({
+        workspace: workspace.id,
+        name: 'RootEntityDefinition',
+        namePlural: 'Root Entity Definition'
+      });
     })
+    .then(persistedRootEntityDefinition=>{
+      rootEntityDefinition = persistedRootEntityDefinition;
+      return CaseDefinition.create({
+        name: 'DemoCase',
+        description: 'Demo Case',
+        summaryPaths: [],
+        entityDefinition: rootEntityDefinition.id
+      });
+    })
+    .then(persistedCaseDefinition =>{      
+      caseDefinition = persistedCaseDefinition;
+      return StageDefinition.create({
+        name: 'IdentificationStage',
+        description: 'Identifictation Stage',
+        isRepeatable: false,
+        isMandatory: true,
+        caseDefinition: caseDefinition.id,
+        parentStageDefinition: null,              
+        newEntityDefinition: null,
+        newEntityAttachPath: null
+      });
+    })
+    .then(persistedStageDefinition=>{
+      stageDefinitioIdentification = persistedStageDefinition;
+      return HumanTaskDefinition.create({
+        name: 'IdentificationTask',
+        description: 'Identification Task',          
+        ownerPath: null,
+        isRepeatable: false,
+        isMandatory: true,
+        caseDefinition: caseDefinition.id,          
+        parentStageDefinition: null,                  
+        newEntityDefinition: null,
+        newEntityAttachPath: null         
+      });
+    })
+    .then(persistedHumanTaskDefinition=>{
+      humanTaskDefinitionIdentification = persistedHumanTaskDefinition;
+      return Promise.resolve();
+    })
+    .then(()=>{
+      return Case.create({
+        caseDefinition: caseDefinition.id
+      });
+    })
+    .then(persistedCase=>{
+      case2 = persistedCase;
+      return Case.findTreebyId(case2.id)
+    })
+    .then(caseTree=>{
+      console.log(JSON.stringify(caseTree,null,2));
+      return Promise.resolve(caseTree);
+    });
   
 });
 
