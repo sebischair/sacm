d399af7a7e73bc4952cb1268f183cbbf5d5ebccd
Felix Michel <felix.michel@tum.de>
2017-06-17 00:45:22 +0200

    fix

--- pug/route.js
+++ pug/route.js
@@ -3,15 +3,11 @@ import pug from 'pug';
 
 
 
-
-
 function routes(){
     const router = express.Router();
 
     router.use('/', (req, res)=>{
-        
-        console.log('pug');
-        res.send(pug.renderFile('pug\\model.pug', {title: "some texit hfex", youAreUsingPug:true}))
+        res.send(pug.renderFile('pug\\model.pug', {itle: "some texit hfex"}))
     });
 
 
