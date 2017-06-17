36ca32c2dabd4c2d8c7c3a9ac8e9e84efd407dde
Felix Michel <felix.michel@tum.de>
2017-06-17 12:30:58 +0200

    api doc included

--- doc2/assets/script.js
+++ doc2/assets/script.js
@@ -1,4 +1,7 @@
 
-  function resizeIframe(obj) {
-    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
-  }
+function resizeIframe(obj) {
+  var wHeight = window.innerHeight;
+  console.log(wHeight)
+  obj.style.height = wHeight-60+'px';
+}
+
