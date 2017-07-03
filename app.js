dbdea8128bc3ebedde8dd1566199822fa31ad3bc
Patrick Holl <patrick.holl@tum.de>
2017-07-03 13:09:16 +0200

    Completed users tests

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, simulateuser");
   next();
 });
 
