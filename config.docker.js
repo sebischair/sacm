d28cc716c756884febf0980dfe89fc83b8e90105
Felix Michel <felix.michel@tum.de>
2017-12-14 16:22:25 +0100

    config test

--- config.docker.js
+++ config.docker.js
@@ -1,3 +1,8 @@
+console.log('ENV')
+console.log(process.env)
+console.log(process.env.SOCIOCORTEX_URL)
+
+
 module.exports = {
     sc : {
         url: process.env.SOCIOCORTEX_URL || 'SocioCortex URL not set!',
