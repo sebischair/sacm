780d77716b704ab0ed043013589424dfc8c31293
Felix Michel <felix.michel@tum.de>
2017-12-14 16:47:37 +0100

    config clean up

--- config.docker.js
+++ config.docker.js
@@ -1,8 +1,3 @@
-console.log('ENV')
-console.log(process.env)
-console.log(process.env.SOCIOCORTEX_URL)
-
-
 module.exports = {
     sc : {
         url: process.env.SOCIOCORTEX_URL || 'SocioCortex URL not set!',
