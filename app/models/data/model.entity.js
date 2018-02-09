934784f3fb29df55d1cd4ed2a05a1dc34f9da9dc
Felix Michel <felix.michel@tum.de>
2018-02-09 21:29:46 +0100

    tree print

--- app/models/data/model.entity.js
+++ app/models/data/model.entity.js
@@ -31,21 +31,36 @@ export default class Entity extends Model{
 
   static entityWithDeepLinksToString(entity, level){
     let s = '';
-    entity.attributes.forEach(attr => {
+    entity.attributes.forEach((attr, i) => {
+      const isLast = entity.attributes.length-1 == i
       if(attr.attributeType == 'link' && attr.attributeTypeConstraints.resourceType == 'entities'){
-        s += this.lineToString(level, '+', attr.description, '');
+        s += this.lineToString(level, this.simpleSymbol(isLast), attr.description, '');
         if(attr.values[0] != null)
           s += this.entityWithDeepLinksToString(attr.values[0], level+1);
       }else if(attr.attributeType == 'link' && attr.attributeTypeConstraints.resourceType == 'users'){
-        s += this.lineToString(level, '+', attr.description, attr.values[0].name);
+        s += this.lineToString(level, this.simpleSymbol(isLast), attr.description, attr.values[0].name);
       }else if(attr.attributeType == 'json'){
-        s += this.lineToString(level, '-', attr.description, attr.values.toString());
+        let value = ''
+        if(attr.values != null);
+          value = JSON.stringify(attr.values);
+        s += this.lineToString(level, this.simpleSymbol(isLast), attr.description, value);
       }else{
-        s += this.lineToString(level, '-', attr.description, attr.values.toString());
+        s += this.lineToString(level, this.simpleSymbol(isLast), attr.description, attr.values.toString());
       }
     });
     return s;
   }
+
+  static simpleSymbol(isLast){
+    if(isLast)
+      return '└─';
+    else
+      return '├─';
+  }
+
+
+
+
 
   static lineToString(level, icon, name, value){
     let spaces = '';
