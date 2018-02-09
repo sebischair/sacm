a04fd6c97e6ed8833e545963f1a35974e30c27a3
Felix Michel <felix.michel@tum.de>
2018-02-09 21:09:15 +0100

    case entity print

--- app/models/data/model.entity.js
+++ app/models/data/model.entity.js
@@ -1,6 +1,7 @@
 import Promise from 'bluebird';
 import http from '../http';
 import Model from '../model';
+import colors from 'colors';
 
 
 export default class Entity extends Model{
@@ -22,26 +23,35 @@ export default class Entity extends Model{
   }
 
   static printEntityWithDeepLinks(entity){
-    console.log('--------------------------');
+    console.log(entity);
+    console.log('--BEGIN-------------------------');
     console.log(this.entityWithDeepLinksToString(entity, 0));
+    console.log('--END---------------------------');
   }
 
   static entityWithDeepLinksToString(entity, level){
     let s = '';
-    let spaces = '';
-    for(let i=0; i<level; i++){
-      spaces += '  ';
-    }
     entity.attributes.forEach(attr => {
       if(attr.attributeType == 'link' && attr.attributeTypeConstraints.resourceType == 'entities'){
-        s += spaces+'+'+attr.description + ' \n'
-        s += this.entityWithDeepLinksToString(attr.values[0], level+1);
+        s += this.lineToString(level, '+', attr.description, '');
+        if(attr.values[0] != null)
+          s += this.entityWithDeepLinksToString(attr.values[0], level+1);
       }else if(attr.attributeType == 'link' && attr.attributeTypeConstraints.resourceType == 'users'){
-        s += spaces+'+'+attr.description +': '+attr.values[0].name+' \n'
-      }else
-        s += spaces+'-'+attr.description + ": " +attr.values.toString()+'\n';
+        s += this.lineToString(level, '+', attr.description, attr.values[0].name);
+      }else if(attr.attributeType == 'json'){
+        s += this.lineToString(level, '-', attr.description, attr.values.toString());
+      }else{
+        s += this.lineToString(level, '-', attr.description, attr.values.toString());
+      }
     });
     return s;
+  }
+
+  static lineToString(level, icon, name, value){
+    let spaces = '';
+    for(let i=0; i<level; i++)
+      spaces += '  ';
+    return spaces+colors.gray(icon + name +': ')+colors.green(value)+' \n'
   }
   
 
