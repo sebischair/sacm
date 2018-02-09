4f597efe4c405f3413ebf38ddd782aa0a5e81469
Felix Michel <felix.michel@tum.de>
2018-02-09 17:10:23 +0100

    print entity with deeplinks

--- app/models/data/model.entity.js
+++ app/models/data/model.entity.js
@@ -14,7 +14,34 @@ export default class Entity extends Model{
   }
 
   static findDeepLinksById(jwt, entityId){
-    return http.get(jwt, '/caseentities/'+entityId+'/deeplinks');
+    return http.get(jwt, '/caseentities/'+entityId+'/deeplinks')
+    .then(entity=>{
+      this.printEntityWithDeepLinks(entity);      
+      return Promise.resolve(entity);
+    });
+  }
+
+  static printEntityWithDeepLinks(entity){
+    console.log('--------------------------');
+    console.log(this.entityWithDeepLinksToString(entity, 0));
+  }
+
+  static entityWithDeepLinksToString(entity, level){
+    let s = '';
+    let spaces = '';
+    for(let i=0; i<level; i++){
+      spaces += '  ';
+    }
+    entity.attributes.forEach(attr => {
+      if(attr.attributeType == 'link' && attr.attributeTypeConstraints.resourceType == 'entities'){
+        s += spaces+'+'+attr.description + ' \n'
+        s += this.entityWithDeepLinksToString(attr.values[0], level+1);
+      }else if(attr.attributeType == 'link' && attr.attributeTypeConstraints.resourceType == 'users'){
+        s += spaces+'+'+attr.description +': '+attr.values[0].name+' \n'
+      }else
+        s += spaces+'-'+attr.description + ": " +attr.values.toString()+'\n';
+    });
+    return s;
   }
   
 
