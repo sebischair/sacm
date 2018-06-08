30e71652910dfc40766cf2b57b0999032e0a0b8b
Felix Michel <felixm87@gmail.com>
2018-06-08 12:25:17 +0200

    israel cs2 automatic test fixes

--- app/importer/importer.js
+++ app/importer/importer.js
@@ -78,7 +78,7 @@ module.exports = class Importer {
         if(userXMLId == null)
           return null
         else if(!this.userMap.has(userXMLId)){
-          console.err('ERROR: User ID "'+userXMLId+'" not found');
+          console.log('ERROR: User ID "'+userXMLId+'" not found');
           return new Error('ERROR: User ID "'+userXMLId+'" not found');
         }else
           return this.userMap.get(userXMLId);  
