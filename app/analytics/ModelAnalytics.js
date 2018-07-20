2d7f2286987ae30fb6719e1129f4bac0c7b8116a
Felix Michel <felixm87@gmail.com>
2018-07-20 15:41:00 +0200

    ignore commits added

--- app/analytics/ModelAnalytics.js
+++ app/analytics/ModelAnalytics.js
@@ -50,20 +50,134 @@ module.exports = class ModelAnalytics{
   }
 
   static async analyzeRepository(){
+    let ignoreList = new Set();
+    ignoreList.add('0f26c2ce4af992b5c20b02038615831e8e6034ee'); //XML parse error, only GCS1 changed
+    ignoreList.add('13c67d4a43bbb49911742f749331a2c4f1f2e89e'); //XML parse error, only GCS1 changed
+    ignoreList.add('bc52e79647fcf4a4a1be15cb73d71733570ad951'); //XML parse error, only GCS1 changed
+    ignoreList.add('26e74f4a2f3e1eb4bd948d7c86a63ae9407ea49d'); //XML parse error, only GCS1 changed
+    ignoreList.add('545b884647d0c9d01897846298f19c60e9e53c25'); //XML parse error, only LCS2 changed
+    ignoreList.add('46973c13092950d2dd6f03e65e2005a9a326bcee'); //XML parse error, only LCS1 changed
+    ignoreList.add('e55993b6c71cd82a2b310633fd73fbb5f79bf5ce'); //XML parse error, only LCS1 changed
+    ignoreList.add('9f7fa8fea07e2c9d0c6ba5bbee81993fffa3dd9e'); //XML parse error, only ICS2 changed
+    ignoreList.add('f8dafe17979040ddcecd30f08488fb8ba2c99358'); //XML parse error, only BCS2 changed
+    ignoreList.add('e48bcce27db754dec1e898c32c6f6919fda858d3'); //XML parse error, only LCS2 changed
+    ignoreList.add('a4d7a99b71fa3aed60b72cdbe2ca87eacf346d55'); //XML parse error, only ICS2 changed
+    ignoreList.add('9c9342b25bf24d1283418b953f4913533f211107'); //XML parse error, only ICS2 changed
+    ignoreList.add('34c94578aece68e812bcb46fa57f3e6826cc4e8c'); //XML parse error, only ICS2 changed
+    ignoreList.add('04079f41ac83377a02288f86f765f916853ccda0'); //XML parse error, only LCS2 changed
+    ignoreList.add('cb02cc78aa800407208c516ca48bac02e96a440a'); //XML parse error, only LCS2 changed
+    ignoreList.add('a3185a2048fb2537470a7d00eef111e5632bcb5f'); //XML parse error, only GCS2 changed
+    ignoreList.add('b0239923afd8b87c5b2c339922ab2054c3c5dd50'); //XML parse error, only GCS2 changed
+    ignoreList.add('e82963081c9a39d5d5add39102eedaf66173094e'); //XML parse error, only GCS2 changed
+    ignoreList.add('0beb6a033c63d94c6feef640676f736b8f190c07'); //XML parse error, only GCS2 changed
+    ignoreList.add('3d35582adb7641128fe8452f98cffd487be3d40d'); //XML parse error, only GCS2 changed
+    ignoreList.add('bce43f31becdfac496ced89c103dff7978c4f0f2'); //XML parse error, only GCS2 changed
+    ignoreList.add('9c967c781239d0777fee67c9880627c617454d02'); //XML parse error, only LCS2 changed
+    ignoreList.add('592f70608e15768d4c9bc04236afb663330258bf'); //XML parse error
+    ignoreList.add('c907ea76f7b57cf4ba4ee93971e65678ad3df7f1'); //XML parse error
+    ignoreList.add('976cb74f910d391578683e1c7ba9ab7cb791d0b9'); //XML parse error
+    ignoreList.add('c70c8d442b05f978b347e4b943f04004f141f4a0'); //XML parse error
+    ignoreList.add('eb30a1907fd4accd879da563de865db6e3cd0f36'); //XML parse error
+    ignoreList.add('b4f64553b5caa6ea9d5a0ddbe4218fa8e62aa0d7'); //XML parse error
+    ignoreList.add('16a48af65f8ad4d3efaf35c5a2f947aedec1d4f4'); //XML parse error
+    ignoreList.add('c87229c7f7fd0d3f3742afcf93279d2b42339898'); //XML parse error
+    ignoreList.add('09c917e8742d84dd3fc15a9a4da74b3c33ff252e'); //XML parse error
+    ignoreList.add('2f661cf3dfda46fd70189d811c3e26eba7f99183'); //XML parse error
+    ignoreList.add('d7e2908aafbb2db64539864633cf4f132fa0bd47'); //XML parse error
+    ignoreList.add('a2ff6d707ea1d4071aa6b8c2db68197370cca96e'); //XML parse error
+    ignoreList.add('d7a7833acf41de2c23670042443d9d058af1198c'); //XML parse error
+    ignoreList.add('9ff7a4292c5f73a0a8ac726b0acb1335b816cef5'); //XML parse error
+    ignoreList.add('e454a79aa60efbf64df6fc7306a59cb6363b39a6'); //XML parse error
+    ignoreList.add('a0e91d291b182b05881add6f394d4f87327e3c26'); //XML parse error
+    ignoreList.add('c5c24fdf1294eeac98ff4ac89be2ae175947c262'); //XML parse error
+    ignoreList.add('50c06420daefd40bb0a8539d5e64d5c2d344f5d0'); //XML parse error
+    ignoreList.add('f3bfcee27ee701366497581e3b4592571730f141'); //XML parse error
+    ignoreList.add('05daec662ffa719285289f3e179cf3cd5ca4dce2'); //XML parse error
+    ignoreList.add('0acc4099c6bf99b53d392865d0c7ddaac971388f'); //XML parse error
+    ignoreList.add('54c3c518fefda064ae453026b56ad624e8b5efbe'); //XML parse error
+    ignoreList.add('de98d0d9cd27416b98a5e358eb62aefd9f6a9165'); //XML parse error
+    ignoreList.add('b5795549ddc96199187c4a575fa28ee8a15788fd'); //XML parse error
+    ignoreList.add('a24c64eb1ccce6870da553468bac7d4608d8f035'); //XML parse error
+    ignoreList.add('6ff4c476e9d6dbfc9da96a17eeccfddde5357241'); //XML parse error
+    ignoreList.add('10532705d782d88024feef30bf4c20308f91bf34'); //XML parse error
+    ignoreList.add('176f83100c6039e14ba2634c9e138439572f4d30'); //XML parse error
+    ignoreList.add('45ebb83043aeea6c334ea93c168a543dc9bbd22e'); //XML parse error
+    ignoreList.add('f84d6c06077ef4b61b4fdb9e68f35f889e669fad'); //XML parse error
+    ignoreList.add('82bfb1a08c4d06eb94771d308f59709aee8c6fae'); //XML parse error
+    ignoreList.add('053f706d6781d65444e74f3201740baeed8fa822'); //XML parse error
+    ignoreList.add('804e36111ad6b1432075fc1dafa9fa5bc432339e'); //XML parse error
+    ignoreList.add('01fba4e95f5a6a27069440a9c2a59e247714b9ef'); //XML parse error
+    ignoreList.add('94cbacc7d2ba3922188d81a3a81508b68cadb148'); //XML parse error
+    ignoreList.add('33e52683b457fc47f492dd15fdcf4841069594c3'); //XML parse error
+    ignoreList.add('f27c00d3db2fe220908c078467c4ff897bd39410'); //XML parse error
+    ignoreList.add('fb764ee2916789b9f29f115e4fd516eea8154792'); //XML parse error
+    ignoreList.add('57b3f20092230710181573c46d7d53aa74f7c0e6'); //XML parse error
+    ignoreList.add('e4592c6e6661d68581e89ff8c5498424fa324fe0'); //XML parse error
+    ignoreList.add('a376e22a47aef9c3bb00833ff8e87c33d692dc43'); //XML parse error
+    ignoreList.add('da48f272920aa58106b6ae1757bdbe7fa9be0669'); //XML parse error
+    ignoreList.add('c3b506554580ff3f9462db3495168daff2070d49'); //XML parse error
+    ignoreList.add('20ff415646708814815720a5b8f9894742d861a9'); //XML parse error
+    ignoreList.add('4ea18cb55c8f292da1d9a56e8cf8432eac80df45'); //XML parse error
+    ignoreList.add('510901cfa4ede6be7cfaabfa7dbd5b78595e30dc'); //XML parse error
+    ignoreList.add('fa909d62f5e43b286b13ab4fa695f6ff62a6176b'); //XML parse error
+    ignoreList.add('da1c9222a83b55401d9485c58ac165534c3ca633'); //XML parse error
+    ignoreList.add('03ae66752da7b5199e1fd0e3ea80b3edd7df98d5'); //XML parse error
+    ignoreList.add('840fdc921cd26a380e595de49297984d211045d0'); //XML parse error
+    ignoreList.add('e429fde88c0259920af8ee98a1494991b4b05840'); //XML parse error
+    ignoreList.add('5fca035ce7de46f49c19cd9bc3c18b248106e198'); //XML parse error
+    ignoreList.add('6d57ac76f8650ef63ccab275af84aff5577f4e5b'); //XML parse error
+    ignoreList.add('039010c58fd8f43b17f869c468db48feb63645c2'); //XML parse error
+    ignoreList.add('f0bc979acbbccd87f5816235a94c9cff3865ba36'); //XML parse error
+    ignoreList.add('2f7eb67038bda2b176a6bb8cf83e5dc6fcdc1dac'); //XML parse error
+    ignoreList.add('b37ecd01bcd11312383dafda4cec14bbc9e41886'); //XML parse error
+    ignoreList.add('61656b8fecfa33fc913bb3f9ace891db3dff7dc9'); //XML parse error
+    ignoreList.add('fbb5a307de645ec726b647f7a3df3099150c2775'); //XML parse error
+    ignoreList.add('b1f54e68423c5c3be52388386906c914e7f40cc4'); //XML parse error
+    ignoreList.add('6c1a7337efba75b0dffdf346e5e46009ba8308db'); //XML parse error
+    ignoreList.add('45ebe1c68f94b243852cd4ad1b3f77e005019c24'); //XML parse error
+    ignoreList.add('6f2311c998c8f8e2b7f628e6b1ff245def01db8f'); //XML parse error
+    ignoreList.add('82200546156efc631e1ecd62fabb9c2e8768d395'); //XML parse error
+    ignoreList.add('40ee23776810dba4ec10c09fbc0ad99f771f46fd'); //XML parse error
+    ignoreList.add('55e10c05b1664bf0fc89a647c30a0b4ab687a20b'); //XML parse error
+    ignoreList.add('e36efd69b5a3f065b72443ab88b18c41cd87da25'); //XML parse error
+    ignoreList.add('bf9dfab6e34cc458b4afdec4294d22ba336d3e3b'); //XML parse error
+    ignoreList.add('ca97cfe20a4a7ead1ab9bd5ff86d3b8b42d788a3'); //XML parse error
+    ignoreList.add('d79d27db975ce0be1731d09b882c9ba684888c36'); //XML parse error
+    ignoreList.add('67811f89fa0a225e0882f05301df2e7785ab6269'); //XML parse error
+    ignoreList.add('fa83fa5e213bb31a4cbeb6bbec8f2329a9d838d1'); //XML parse error
+    ignoreList.add('7d48fee1b770b383a4d54feb7a0be95418e979d5'); //XML parse error
+    ignoreList.add('990519e62935d2e31ecdc0aeea814204a75b5629'); //XML parse error
+    ignoreList.add(''); //XML parse error
+    ignoreList.add(''); //XML parse error
+    ignoreList.add(''); //XML parse error
+    ignoreList.add(''); //XML parse error
+    ignoreList.add(''); //XML parse error
+    ignoreList.add(''); //XML parse error
+    ignoreList.add(''); //XML parse error
+
     let result = [];
-    try{
+   // try{
       await Git.checkout(['master']);
       console.log('git checkout master completed')
       let data = await Git.log(['-m', '--follow', '*.xml']);
       console.log('git log completed - '+data.all.length + ' matching commits!')
       let allFilePaths = new Set();
+      let isStarted = false;
       for(let i=0; i<data.all.length; i++){
-
+        
         let c = data.all[i];
+        if(c.hash == '990519e62935d2e31ecdc0aeea814204a75b5629')
+          isStarted = true;
+        if(!isStarted)
+          continue;
+        if(ignoreList.has(c.hash))
+          continue;
+
         await Git.checkout([c.hash]);
         console.log('\n'+(i+1)+'/'+data.all.length +' checkout '+c.hash+' completed! ');
         console.log('---Message: '+c.message)
         console.log('---Date: '+c.date)
+        console.log('---Author Name: '+c.author_name)
         let findPath = repositoryPath;
         if(await fs.exists(repositoryPath+'/app'))  
           findPath +='/app';
@@ -75,7 +189,6 @@ module.exports = class ModelAnalytics{
 
         files = this.filterFiles(files);
         console.log('---found '+files.length+' files!');
-        console.log(c);
         let commitResult = {
           hash: c.hash,
           date: c.date,
@@ -93,17 +206,17 @@ module.exports = class ModelAnalytics{
           });
         }
         result.push(commitResult);
-        if(i==2)
-          break;
+        //if(i==100)
+         // break;
         
       }
       console.log('\nSet of all repository file names:');
       console.log(allFilePaths)
 
       await this.saveAsExcel(result);
-    }catch(e){
-      console.log(e);
-    }    
+    //}catch(e){
+    //  console.log(e);
+   // }    
     return result;
   }
 
@@ -243,19 +356,20 @@ module.exports = class ModelAnalytics{
 
   static async analyzeFile(filePath){   
     let result = {};
-    try{
+    //try{
       //const filePath = 'app/importer/studyrelease.case.groningen.cs2.xml';
       const fileContent = fs.readFileSync(filePath).toString();
-      const xml = await xml2js.parseStringAsync(fileContent, {explicitChildren:true, preserveChildrenOrder:true});     
+      const xml = await xml2js.parseStringAsync(fileContent, {explicitChildren:true, preserveChildrenOrder:true});  
       const Workspace =  xml.SACMDefinition.Workspace[0];
 
-      result.attributeDefinitions = this.analyzeAttributeDefinitions(Workspace);      
+      result.attributeDefinitions = this.analyzeAttributeDefinitions(Workspace);    
       result.derivedAttributeDefinitions = this.analyzeDerivedAttributeDefinitions(Workspace);
       result.entityDefinitions = this.analyzeEntityDefinitions(Workspace);
       result.stageDefinitions = this.analyzeStageDefinitions(Workspace);
+   /*
     }catch(e){
       console.log(e);
-    }
+    }*/
     return result;
   }
 
